0f354490e01b5d00dd2da8524829175cfcbe73f5
helloworld <helloworld@ns.com>
2023-01-21 23:53:36 +0800

    add c06

---
+++ c06/src/config.h
@@ -0,0 +1,12 @@
+#ifndef __CONFIG_H_
+#define __CONFIG_H_
+
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+// const int frame_rate = 200;
+const int frame_rate = 60;
+
+
+#endif //!__CONFIG_H_
